fe4d5f3fe8a520700418b17e39df4eefe1b4019d
zfhaysia <116173829+zfhaysia@users.noreply.github.com>
2024-06-25 02:00:12 +0800

    Draft

--- app/src/main/java/com/example/signuploginrealtime/RegisterPetActivity.java
+++ app/src/main/java/com/example/signuploginrealtime/RegisterPetActivity.java
@@ -18,19 +18,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
             // Find the Spinner by ID
             Spinner petGenderSpinner = findViewById(R.id.register_petGender);
+            Spinner petCategorySpinner = findViewById(R.id.register_petCategory);
 
             // Create an ArrayAdapter using the string array and a default spinner layout
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
                     R.array.gender_array, android.R.layout.simple_spinner_item);
+            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                    R.array.category_array, android.R.layout.simple_spinner_item);
 
             // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
             // Apply the adapter to the spinner
-            petGenderSpinner.setAdapter(adapter);
+            petGenderSpinner.setAdapter(adapter1);
+            petCategorySpinner.setAdapter(adapter2);
 
             // Set an item selected listener for the spinner
             petGenderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    if (position > 0) {
+                        String selectedItem = parent.getItemAtPosition(position).toString();
+                        Toast.makeText(RegisterPetActivity.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    // Another interface callback
+                }
+            });
+
+            petCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                     if (position > 0) {
